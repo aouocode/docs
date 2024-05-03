11097a8267b5dfc37c8366628b3263c3700f6658
aouos <aouos@qq.com>
2024-05-03 10:03:49 +0800

    init feature website

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import Home from './views/home/index.vue';
+
+const menus = ['Home', 'Blog', 'Works', 'About'];
+</script>
+
+<template>
+  <div class="main">
+    <div class="header">
+      <div class="logo">
+        <a href="/"></a>
+      </div>
+
+      <div class="menus">
+        <template v-for="menu in menus">
+          <span>{{ menu }}</span>
+        </template>
+      </div>
+    </div>
+
+    <div class="container">
+      <Home />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  width: 100%;
+  height: 100%;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  height: 60px;
+  font-weight: bold;
+}
+
+.menus {
+  display: flex;
+  gap: 16px;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.container {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 60px);
+}
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div class="home">
+    <div class="left"></div>
+    <div class="right">
+      <img src="../../assets/me.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 1200px;
+}
+
+.left,
+.right {
+  width: 50%;
+}
+
+img {
+  width: 100%;
+}
+</style>
